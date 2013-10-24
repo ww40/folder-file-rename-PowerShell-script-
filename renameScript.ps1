7895b62e848c0289e34660b897cb3caad5c25d22
William T. Webb <eyezopen@gmail.com>
2013-10-24 14:13:39 +0100

    added notes
    added author, name of script and its purpose with 2 descriptions upon
    the code.

---
+++ renameScript.ps1
@@ -0,0 +1,13 @@
+﻿######################################################################
+## William T. Webb													##
+## Renaming Script													##
+## purpose: renames folders/files and replaces spaces with _		##
+######################################################################
+
+
+## script that when run in a particular directory it will rename all the folders first and replace spaces with _
+Get-ChildItem -Directory -r | where { $_.Name -match "\s" } | rni -newname { $_.Name -replace "\s","_" }
+
+## script again to check all the items within the folders replacing spaces with _
+Get-ChildItem -File -r | where { $_.Name -match "\s" } | rni -newname { $_.Name -replace "\s","_" }
+
